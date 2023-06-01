c53143b305bd0d285d1acb710c572116d49b7542
hyperher <mihernan@live.com>
2023-06-01 09:19:47 +0200

    ss
    ss

---
+++ Scripts/CrearDC.ps1
@@ -0,0 +1,16 @@
+﻿#set static IP address 
+$ipaddress = “192.168.1.238” 
+$ipprefix = “24” 
+$ipgw = “192.168.1.244” 
+$ipdns = “127.0.0.1” 
+$ipif = (Get-NetAdapter).ifIndex 
+New-NetIPAddress -IPAddress $ipaddress -PrefixLength $ipprefix -InterfaceIndex $ipif -DefaultGateway $ipgw
+
+#rename the computer 
+$newname = “DcPing” 
+Rename-Computer -NewName $newname –force
+
+
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+
+Install-ADDSForest -DomainName "dominio.local" -DomainNetBiosName "domain" -InstallDns:$true 
